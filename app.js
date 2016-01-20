f13d00136f2130d89a0be038049913bce3f0bd6f
stanleyyork <stanleyyork@gmail.com>
2016-01-20 12:09:07 -0800

    init commit

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var app = angular.module('libraryApp', []);
+var app = angular.module('libraryApp', ['ngRoute']);
 
 ////////////
 // ROUTES //
 ////////////
 
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider)  {
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
-      template: 'Home!'
+      templateUrl: 'templates/books/index.html',
+      controller: 'BooksIndexCtrl'
+    })
+    .when('/books/:id', {
+      templateUrl: 'templates/books/show.html',
+      controller: 'BooksShowCtrl'
+    });
+    $locationProvider
+    .html5Mode({
+      enabled: true,
+      requireBase: false
     });
 }]);
 
@@ -17,8 +27,13 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 
 app.controller('BooksIndexCtrl', ['$scope', function ($scope) {
   $scope.booksIndexTest = 'Connected to BooksIndexCtrl';
+  $scope.allBooks = allBooks;
 }]);
 
-app.controller('BooksShowCtrl', ['$scope', function ($scope) {
+app.controller('BooksShowCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
   $scope.booksShowTest = 'Connected to BooksShowCtrl';
+  var bookId = $routeParams.id;
+  $scope.book = allBooks.filter(function( book ) {
+	  return book._id == bookId;
+	});
 }]);
